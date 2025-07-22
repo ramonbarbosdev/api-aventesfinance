a8e332fef03e89ef6a304d34aff2a8e5e1e4725e
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-07-22 11:58:03 -0300

    cors3

--- src/main/java/com/api_aventesfinance/security/WebConfigSecurity.java
+++ src/main/java/com/api_aventesfinance/security/WebConfigSecurity.java
@@ -1,6 +1,5 @@
 package com.api_aventesfinance.security;
 
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,7 +28,6 @@ import com.api_aventesfinance.service.ImplementacaoUserDetailsService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity {
@@ -37,33 +35,33 @@ public class WebConfigSecurity {
     @Autowired
     private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 
- 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-      
-    	return http  
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> csrf.disable())  
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll() 
-                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll() 
-                    .requestMatchers(HttpMethod.POST, "/auth/logout").permitAll() 
-                    .requestMatchers(HttpMethod.GET, "/status/").permitAll() 
-            		.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-            		.requestMatchers(HttpMethod.OPTIONS,"/index").permitAll()
-            		// .requestMatchers(HttpMethod.OPTIONS,"/chat-socket/**").permitAll()
-                    .requestMatchers(
-                        new AntPathRequestMatcher("/swagger-ui/**"),
-                        new AntPathRequestMatcher("/v3/api-docs/**"),
-                        new AntPathRequestMatcher("/swagger-ui.html"),
-                        new AntPathRequestMatcher("/swagger-resources/**"),
-                        new AntPathRequestMatcher("/webjars/**")
-                    ).permitAll()
-                    .anyRequest().authenticated() 
-            )
-            .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class)
-            // .addFilterBefore(new JWTLoginFilter("/login", authenticationManager), UsernamePasswordAuthenticationFilter.class)
-            .build();  
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager)
+            throws Exception {
+
+        return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/logout").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/status/").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/index").permitAll()
+                        // .requestMatchers(HttpMethod.OPTIONS,"/chat-socket/**").permitAll()
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/swagger-ui/**"),
+                                new AntPathRequestMatcher("/v3/api-docs/**"),
+                                new AntPathRequestMatcher("/swagger-ui.html"),
+                                new AntPathRequestMatcher("/swagger-resources/**"),
+                                new AntPathRequestMatcher("/webjars/**"))
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class)
+                // .addFilterBefore(new JWTLoginFilter("/login", authenticationManager),
+                // UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
 
@@ -73,22 +71,26 @@ public class WebConfigSecurity {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://avantes.ramoncode.com.br", "https://apiavantes.ramoncode.com.br")); // Libera o Angular
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:4200",
+                "https://avantes.ramoncode.com.br",
+                "https://apiavantes.ramoncode.com.br",
+                "https://apiavantes.ramoncode.com.br:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true); 
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-   
 }
